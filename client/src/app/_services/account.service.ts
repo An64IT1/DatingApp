e67aac46776495badc185de6227a1051e58ab96c
An64IT1 <an0994305@gmail.com>
2022-08-06 00:23:49 +0700

    9/15 chapter 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSouce = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSouce.asObservable();
+
+  constructor(private http: HttpClient) { }
+  
+  login(model: User){
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSouce.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSouce.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSouce.next(null);
+  }
+}
