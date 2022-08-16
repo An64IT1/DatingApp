1b3b82dabe7733a2ed05f8934d4dec8e077d857c
An64IT1 <an0994305@gmail.com>
2022-08-16 23:39:55 +0700

    13/19 chapter13

---
+++ API/API/Helpers/IPaginationHeader.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Helpers
+{
+    public interface IPaginationHeader
+    {
+        int CurrentPage { get; set; }
+        int ItemsPerPage { get; set; }
+        int TotalItems { get; set; }
+        int TotalPages { get; set; }
+    }
+}
